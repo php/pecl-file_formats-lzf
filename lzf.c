a62af9962af9b3710656393fe2aaef7804329ccc
Marcin Gibula <mg@php.net>
2009-03-02 14:53:09 +0000

    Fixed crash in phpinfo()

--- lzf.c
+++ lzf.c
@@ -76,7 +76,7 @@ PHP_MINFO_FUNCTION(lzf)
 	php_info_print_table_start();
 	php_info_print_table_header(2, "lzf support", "enabled");
 	php_info_print_table_row(2, "version", PHP_LZF_VERSION);
-	php_info_print_table_row(2, "lzf API version", LZF_VERSION);
+	/*php_info_print_table_row(2, "lzf API version", LZF_VERSION);*/
 	#if PHP_LZF_ULTRA_FAST
 	php_info_print_table_row(2, "optimized for", "speed");
 	#else
